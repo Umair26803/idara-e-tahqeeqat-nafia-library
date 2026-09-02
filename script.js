a17a9638262406e0cfb9afb3659e45017d23aed4
Umair Ahmad <uamirahmad26803@gmail.com>
2026-09-02 15:22:53 +0500

    Add button click interaction

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Library website is running!");
+const addBookButton = document.querySelector(".primary-btn");
+
+addBookButton.addEventListener("click", function() {
+    alert("Add New Book clicked!");
+});
