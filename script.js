e8030c126e80786cd7a89eef2fe448a07f6c94df
Umair Ahmad <uamirahmad26803@gmail.com>
2026-09-02 15:43:00 +0500

    Add DOM interaction for new book button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 console.log("Library website is running!");
-const addBookButton = document.querySelector(".primary-btn");
+
+const addBookButton = document.querySelector("#addBookButton");
+const bookMessage = document.querySelector("#bookMessage");
 
 addBookButton.addEventListener("click", function() {
-    alert("Add New Book clicked!");
+    bookMessage.textContent = "Ready to add a new book!";
 });
