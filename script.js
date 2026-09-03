ff66715a199e7a2a326f362a66eaa66df7eccaf9
Umair Ahmad <uamirahmad26803@gmail.com>
2026-09-03 12:24:03 +0500

    Add books to array

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("Library website is running!");
-let books = ["Atomic Habits", "The Alchemist", "Rich Dad Poor Dad"];
+let books = ["Atomic Habits", "The Alchemist", "Rich Dad Poor Dad", "The Psychology Of Money", "Deep Work"];
 
 const addBookButton = document.querySelector("#addBookButton");
 const bookMessage = document.querySelector("#bookMessage");
@@ -7,10 +7,12 @@ const bookTitleInput = document.querySelector("#bookTitle");
 
 addBookButton.addEventListener("click", function() {
     addBookButton.addEventListener("click", function() {
-    
+
         if (bookTitleInput.value !== "") {
-            bookMessage.textContent = bookTitleInput.value;
-        }else {
+            books.push(bookTitleInput.value);
+            console.log(books);
+            bookMessage.textContent = bookTitleInput.value + " added to library!";
+        } else {
             bookMessage.textContent = "Please enter a book title.";
         }
     
