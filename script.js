8b3482d977c504893ef7c30b15f0e91437ef1745
Umair Ahmad <uamirahmad26803@gmail.com>
2026-09-03 13:18:19 +0500

    Fix book button event listener

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("Library website is running!");
+
 let books = ["Atomic Habits", "The Alchemist", "Rich Dad Poor Dad"];
 
 const addBookButton = document.querySelector("#addBookButton");
@@ -6,15 +7,12 @@ const bookMessage = document.querySelector("#bookMessage");
 const bookTitleInput = document.querySelector("#bookTitle");
 
 addBookButton.addEventListener("click", function() {
-    addBookButton.addEventListener("click", function() {
 
-        if (bookTitleInput.value !== "") {
-            books.push(bookTitleInput.value);
-            console.log(books);
-            bookMessage.textContent = bookTitleInput.value + " added to library!";
-        } else {
-            bookMessage.textContent = "Please enter a book title.";
-        }
-    
-    });
+    if (bookTitleInput.value !== "") {
+        books.push(bookTitleInput.value);
+        bookMessage.textContent = bookTitleInput.value + " added to library!";
+    } else {
+        bookMessage.textContent = "Please enter a book title.";
+    }
+
 });
