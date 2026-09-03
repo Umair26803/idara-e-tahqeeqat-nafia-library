99b24e4f87e1f0762e6ecadec187b2026cba4dc8
Umair Ahmad <uamirahmad26803@gmail.com>
2026-09-03 13:01:28 +0500

    Learn to find the last book in array

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 console.log("Library website is running!");
 let books = ["Atomic Habits", "The Alchemist", "Rich Dad Poor Dad"];
 
-
 const addBookButton = document.querySelector("#addBookButton");
 const bookMessage = document.querySelector("#bookMessage");
 const bookTitleInput = document.querySelector("#bookTitle");
