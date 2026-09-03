e7ba826091bc17d3ce6ab26718ec8d4054ffe30e
Umair Ahmad <uamirahmad26803@gmail.com>
2026-09-03 07:15:16 +0500

    Learn JavaScript variables

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const addBookButton = document.querySelector("#addBookButton");
 const bookMessage = document.querySelector("#bookMessage");
 const bookTitleInput = document.querySelector("#bookTitle");
 
+let bookTitle = "Atomic Habits";
+
 addBookButton.addEventListener("click", function() {
     bookMessage.textContent = bookTitleInput.value;
 });
