60fb5381fa4834c49c015d066f1cb46b517fd0b4
Umair Ahmad <uamirahmad26803@gmail.com>
2026-09-07 12:25:06 +0500

    Convert books to objects

--- script.js
+++ script.js
@@ -1,10 +1,80 @@
 console.log("Library website is running!");
 
 let books = [
-    "Talk to Allah",
-    "The Alchemist",
-    "Rich Dad Poor Dad",
+    {
+        title: "Talk to Allah",
+        author: "Ayesha Syahire",
+        category: "Islamic Studies"
+    },
+
+    {
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        category: "Fiction"
+    },
+
+    {
+        title: "Rich Dad Poor Dad",
+        author: "Robert Kiyosaki",
+        category: "Finance"
+    }
 ];
+
+let students = [
+    {
+        name: "Ali",
+        age: 20
+    },
+
+    {
+        name: "Ahmed",
+        age: 22
+    }
+];
+
+console.log(students);
+console.log(students[0]);
+console.log(students[0].name);
+
+let libraryBooks = [
+    {
+        title: "Deep Work",
+        author: "Cal Newport",
+        category: "Productivity"
+    },
+
+    {
+        title: "Atomic Habits",
+        author: "James Clear",
+        category: "Self Development"
+    },
+
+    {
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        category: "Fiction"
+    }
+];
+
+console.log(libraryBooks);
+console.log(libraryBooks[0].title);
+console.log(libraryBooks[1].author);
+
+let testBooks = [
+    {
+        title: "Deep Work",
+        author: "Cal Newport"
+    },
+
+    {
+        title: "Atomic Habits",
+        author: "James Clear"
+    }
+];
+
+console.log(testBooks);
+console.log(testBooks[0].title);
+console.log(testBooks[1].author);
 
 const addBookButton = document.querySelector("#addBookButton");
 const bookMessage = document.querySelector("#bookMessage");
@@ -15,9 +85,10 @@ function displayBooks() {
     bookList.innerHTML = "";
 
     for (let book of books) {
-        bookList.innerHTML += '<p class="dynamic-book">' + book + "</p>";
+        bookList.innerHTML += '<p class="dynamic-book">' + book.title + "</p>";
     }
 }
+
 displayBooks();
 
 addBookButton.addEventListener("click", function() {
