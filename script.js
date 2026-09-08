12d28f426e3ca7b152242afa3a567ba6b6106b22
Umair Ahmad <uamirahmad26803@gmail.com>
2026-09-08 08:09:32 +0500

    Display book author and category

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function displayBooks() {
     bookList.innerHTML = "";
 
     for (let book of books) {
-        bookList.innerHTML += '<p class="dynamic-book">' + book.title + "</p>";
+        bookList.innerHTML += '<p class="dynamic-book">' + book.title + "<br>" + book.author + "<br>" + book.category + "</p>";
     }
 }
 
